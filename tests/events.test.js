0b18c5806c6b007c66d4e239df9cb1871642708f
Fabricio Veliz <fabricio.damian.veliz@gmail.com>
2021-08-08 19:40:55 -0300

    cambios

--- tests/events.test.js
+++ tests/events.test.js
@@ -37,52 +37,6 @@ describe('obtener todos los eventos', () => {
     });
 });
 
-describe('crear un evento', () => {
-    test('se puede agregar un evento válido', async () => {
-    
-        const newEvent = {
-            title : "EVENTO CUATRO",
-            description : "Su descripción",
-            dateList : ["2022-07-07T22:44:33.081+00:00", "2022-07-08T22:44:33.081+00:00"],
-            place : "Su ubicación",
-            outstanding : false,
-            image : "http://#"
-        }
-    
-        await api
-            .post('/api/events-login')
-            .send(newEvent)
-            .expect(201)
-            .expect('Content-Type', /application\/json/);
-        
-        const response = await api.get('/api/events');
-        const titles = response.body.map(event => event.title);
-        expect(response.body).toHaveLength(initialEvents.length+1);
-        expect(titles).toContain('EVENTO CUATRO');
-        
-    });
-    
-    test('no se puede agregar un evento válido', async () => {
-        
-        const newEvent = {
-            title : "EVENTO CINCO",
-            dateList : ["2022-07-09T22:44:33.081+00:00", "2022-07-10T22:44:33.081+00:00"],
-            place : "Su ubicación",
-            outstanding : false,
-            image : "http://#"
-        }
-    
-        await api
-            .post('/api/events-login')
-            .send(newEvent)
-            .expect(400)
-        
-        const response = await api.get('/api/events');
-        expect(response.body).toHaveLength(initialEvents.length);
-    
-    });
-});
-
 afterAll(() => {
     mongoose.connection.close();
     server.close();
